185e5b30359d06b887a1944e10fe35a3143bbdff
Arleta Kasprzyk-Muzusa <arletakasp@gmail.com>
2024-01-22 16:00:33 +0100

    Add Basic Options Functionality

--- src/main/java/pl/isa/freshmenindustries/Main.java
+++ src/main/java/pl/isa/freshmenindustries/Main.java
@@ -1,4 +1,6 @@
 package pl.isa.freshmenindustries;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +26,7 @@ public class Main {
 
                 if (haslo.equals(zarejestrowaniUzytkownicy.get(nazwaUzytkownika))) {
                     System.out.println("Witaj " + nazwaUzytkownika + ".");
+                    mainMenu();
                     break;
                 } else {
                     System.out.println("Nieprawidłowe hasło. Spróbuj ponownie.");
@@ -33,5 +36,73 @@ public class Main {
             }
         }
         scanner.close();
+    }
+
+    static void mainMenu() {
+        BasicOptions basicOptions = new BasicOptions();
+        basicOptions.displayOptions();
+
+        System.out.println(basicOptions.enterNumber());
+        try {
+            Scanner scanner = new Scanner(System.in);
+            int number = scanner.nextInt();
+            switch (number) {
+                case 1:
+                    System.out.println(basicOptions.chooseOption(number));
+                    System.out.println("You started a new game");
+                    break;
+                case 2:
+                    System.out.println(basicOptions.chooseOption(number));
+                    System.out.println("You are in manage games menu");
+                    break;
+                default:
+                    while (number != 1 || number != 2) {
+                        System.out.println("Choose again. Enter the number 1 or 2:");
+                        Scanner scanner1 = new Scanner(System.in);
+                        int newNumber = scanner1.nextInt();
+                        if (newNumber == 1) {
+                            System.out.println(basicOptions.chooseOption(newNumber));
+                            System.out.println("You started a new game");
+                            break;
+                        }
+                        if (newNumber == 2) {
+                            System.out.println(basicOptions.chooseOption(newNumber));
+                            System.out.println("You are in manage games menu");
+                            break;
+                        }
+                    }
+            }
+        } catch (InputMismatchException exc) {
+            System.out.println("You may only enter natural number. Enter 1 or 2: ");
+            Scanner scanner = new Scanner(System.in);
+            int number = scanner.nextInt();
+
+            switch (number) {
+                case 1:
+                    System.out.println(basicOptions.chooseOption(number));
+                    System.out.println("You started a new game");
+                    break;
+                case 2:
+                    System.out.println(basicOptions.chooseOption(number));
+                    System.out.println("You are in manage games menu");
+                    break;
+                default:
+                    while (number != 1 || number != 2) {
+                        System.out.println("Choose again. Enter the number 1 or 2:");
+                        Scanner scanner1 = new Scanner(System.in);
+                        int newNumber = scanner1.nextInt();
+                        if (newNumber == 1) {
+                            System.out.println(basicOptions.chooseOption(newNumber));
+                            System.out.println("You started a new game");
+                            break;
+                        }
+                        if (newNumber == 2) {
+                            System.out.println(basicOptions.chooseOption(newNumber));
+                            System.out.println("You are in manage games menu");
+                            break;
+                        }
+                    }
+            }
+        }
     }
 }
