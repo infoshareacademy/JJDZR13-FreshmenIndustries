847d2251551fd88ea61096233c7cd0a3d76a22f0
Arleta Kasprzyk-Muzusa <arletakasp@gmail.com>
2024-02-10 10:09:04 +0100

    Game edition changes 3

--- src/main/java/pl/isa/freshmenindustries/game/Game.java
+++ src/main/java/pl/isa/freshmenindustries/game/Game.java
@@ -13,19 +13,17 @@ public class Game {
         return name;
     }
 
-    public String setName(String name) {
+    public void setName(String name) {
         this.name = name;
-        return name;
-    }
+        }
 
     public String getDescription() {
         return description;
     }
 
-    public String setDescription(String description) {
+    public void setDescription(String description) {
         this.description = description;
-        return description;
-    }
+        }
 
 }
 
