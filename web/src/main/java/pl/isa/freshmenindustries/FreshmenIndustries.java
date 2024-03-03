10300b2bbbe2819071c19d7d16c955af41507bae
Tomasz Odwald <tomasz.odwald@gmail.com>
2024-03-03 19:22:16 +0100

    Moved console app to separate module

---
+++ web/src/main/java/pl/isa/freshmenindustries/FreshmenIndustries.java
@@ -0,0 +1,13 @@
+package pl.isa.freshmenindustries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshmenIndustries {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshmenIndustries.class, args);
+	}
+
+}
---
+++ web/src/test/java/pl/isa/freshmenindustries/FreshmenIndustriesTests.java
@@ -0,0 +1,13 @@
+package pl.isa.freshmenindustries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshmenIndustriesTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
