38b6338f4e24172da82db733a8b04ab5b75ba7f0
Arleta Kasprzyk-Muzusa <arletakasp@gmail.com>
2024-04-07 21:03:11 +0200

    Merge with main

--- web/src/main/java/pl/isa/freshmenindustries/game/GameController.java
+++ web/src/main/java/pl/isa/freshmenindustries/game/GameController.java
@@ -21,16 +21,6 @@ public class GameController {
         this.gameService = gameService;
     }
 
-    @GetMapping("start-new-game")
-    public String startNewGame(Model model,
-                               @ModelAttribute("response") Response response,
-                               @ModelAttribute("game") Game game) {
-        model.addAttribute("gamesList", gameService.getAllGames().getData())
-                .addAttribute("response", response)
-                .addAttribute("game", game)
-                .addAttribute("content", "startNewGame");
-        return "main";
-    }
     @GetMapping("/manage-games")
     public String games(Model model,
                         @ModelAttribute("response") Response response,
@@ -47,6 +37,16 @@ public class GameController {
         return new ResponseEntity<>(gameService.getGameById(id), HttpStatus.OK);
     }
 
+    @GetMapping("start-new-game")
+    public String startNewGame(Model model,
+    @ModelAttribute("response") Response response,
+    @ModelAttribute("game") Game game) {
+        model.addAttribute("gamesList", gameService.getAllGames().getData())
+                .addAttribute("response", response)
+                .addAttribute("game", game)
+                .addAttribute("content", "startNewGame");
+        return "main";
+    }
 
     @PostMapping("/manage-games/create")
     public String createGame(@ModelAttribute Game game, RedirectAttributes redirectAttributes) {
